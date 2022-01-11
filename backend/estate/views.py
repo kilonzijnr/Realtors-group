eba50a7138d274c2bca562e0f4d27876e11731d7
patrick mugambi <patrick@kali.pm.com>
2022-01-11 14:07:39 +0300

    create property view logic

--- backend/estate/views.py
+++ backend/estate/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from forms import PropertyForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+def add_roperty(request):
+    """[property functiom]
+
+    Args:
+        request ([function]): [function to create new property]
+    """
+    if request.method == "POST":
+        form = PropertyForm(request.POST,request.FILES)
+        if form.is_valid():
+            new_prop = form.save(commit=False)
+            new_prop.save()
+        return redirect("home")
+    else:
+        form = PropertyForm()
+    return render(request, "property/new_property.html", {"form": form})
