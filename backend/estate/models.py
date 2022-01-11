dc29b866d2571853c528b69a5eacd72e9222924a
patrick mugambi <patrick@kali.pm.com>
2022-01-11 12:07:30 +0300

    create property type model

--- backend/estate/models.py
+++ backend/estate/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class PropertyType(models.Model):
+    """[Property type model]
+
+    Args:
+        models ([type]): [Type of property model]
+    """
