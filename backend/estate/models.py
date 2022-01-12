17ea90ed9404dd0441dd1834c3f2672c193f2010
patrick mugambi <patrick@kali.pm.com>
2022-01-12 20:21:24 +0300

    create comment model

--- backend/estate/models.py
+++ backend/estate/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 category_choices = (
@@ -39,3 +40,16 @@ class Property(models.Model):
         return Property.objects.filter(property=self).count()
 
 
+class Comment(models.Model):
+    post = models.ForeignKey('Property', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
