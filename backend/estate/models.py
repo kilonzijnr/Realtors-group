6a1e7e85672c8a94b7ed2463fff045b9537a3b1f
0724654276 <you@example.com>
2022-01-12 11:39:20 +0300

    modified model class rating

--- backend/estate/models.py
+++ backend/estate/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Rating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    property = models.ForeignKey(property, on_delete=models.CASCADE)
     review = models.TextField(null=True)
     rate_property = models.PositiveSmallIntegerField(choices = RATE_CHOICES)
     
