725d291c37cd41c8c0e4e245d42da62d52c7fae5
0724654276 <you@example.com>
2022-01-12 11:54:34 +0300

    create method to calculate average rate

--- backend/estate/models.py
+++ backend/estate/models.py
@@ -11,7 +11,16 @@ class Rating(models.Model):
         ratings = Rating.objects.filter(project=self)
         return len(ratings)
 
-    
+    def average_ratings(self):
+        sum = 0
+        ratings = Rating.objects.filter(project=self)
+        for rating in ratings:
+            sum += ((rating.rate_design + rating.rate_usability + rating.rate_content)/3)
+        if len(ratings) > 0:
+            return sum/len(ratings)
+        else:
+            return 0
+
 
 def __str__(self):
         return self.user.username
