7189febd3e83fa4d2b43346368ad06ccb7a9ed44
synthiakageni <synthia.kageni@student.moringaschool.com>
2022-01-12 12:41:43 +0300

    import login_required"

--- backend/estate/views.py
+++ backend/estate/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required 
 # Create your views here.
+@login_required(login_url='login')
+def search_profile(request):
+    if 'search_user' in request.GET and request.GET['search_user']:
+        name = request.GET.get("search_user")
+        results = Profile.search_profile(name)
+        print(results)
+        message = f'name'
+        params = {
+            'results': results,
+            'message': message
+        }
+        return render(request, 'results.html', params)
+    else:
+        message = "You haven't searched for any  category"
+    return render(request, 'results.html', {'message': message})
