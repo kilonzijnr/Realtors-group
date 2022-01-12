3b48fd87f9447ffb71b7fa2a4a25266440b83142
kilonzi <vkilonzi05@gmail.com>
2022-01-12 11:19:33 +0300

    created user registration functionality

--- backend/estate/views.py
+++ backend/estate/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, request
+from .forms import CreateUserForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.models import Group
 
 # Create your views here.
+def register(request):
+    """User Registration Functionality"""
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            group = Group.objects.get(name='Customers')
+            user.groups.add(group)
+            return redirect('user-login')
+    else:
+        form = CreateUserForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'register.html', context)
