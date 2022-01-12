6dedfaaae0a32f529c022a712d1af90cd552a6ec
kilonzi <vkilonzi05@gmail.com>
2022-01-12 11:11:21 +0300

    created user registration form

---
+++ backend/estate/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Profile
+
+#create your forms below
+
+class CreateUserForm(UserCreationForm):
+    """User Sign-up Form"""
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
