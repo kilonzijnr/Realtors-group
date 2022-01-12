6a765514f6ee63479b4870f65923a644aaf0c8f4
patrick mugambi <patrick@kali.pm.com>
2022-01-12 11:18:11 +0300

    register blog and comment models in admin.py

--- backend/estate/admin.py
+++ backend/estate/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Property
+#comment model
+from .models import Blog,Comment
 # Register your models here.
 
 admin.site.register(Property)
+admin.site.register(Blog)
+admin.site.register(Comment)
