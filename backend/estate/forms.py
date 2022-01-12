2b2dce5debc7c52693118629e024bc8b18ab5ad0
kilonzi <vkilonzi05@gmail.com>
2022-01-12 11:13:55 +0300

    created a form  for user update

--- backend/estate/forms.py
+++ backend/estate/forms.py
@@ -11,3 +11,8 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
