09b1f0bd7ab7c66059ef68d7f8d2bd91b687ae75
shawn <shawn@kali.shawn>
2022-01-12 00:40:59 -0900

    add cities

--- backend/cities/views.py
+++ backend/cities/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import City
+
+
+class CitiesDetailView(DetailView):
+    """
+        City detail view.
+    """
+    template_name = 'cities/city-detail.html'
+    model = City
 
 # Create your views here.
