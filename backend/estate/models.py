e41f1752a7e4d38ea6b7494c8ba4de5584c1b586
0724654276 <you@example.com>
2022-01-12 11:56:32 +0300

    create a rating scale

--- backend/estate/models.py
+++ backend/estate/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+RATE_CHOICES = [
+    (10,'10-Outstanding'),
+    (9,'9-Exceeds Expectations'),
+    (8,'8-Excellent'),
+    (7,'7-Good'),
+    (6,'6-Barely Above Average'),
+    (5,'5-Average'),
+    (4,'4-Poor'),
+    (3,'3-Awful'),
+    (2,'2-Dreadful'),
+    (1,'1-Troll'),
+]
+
 class Rating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     property = models.ForeignKey(property, on_delete=models.CASCADE)
     review = models.TextField(null=True)
     rate_property = models.PositiveSmallIntegerField(choices = RATE_CHOICES)
+
+    
     
     def no_of_ratings(self):
         ratings = Rating.objects.filter(project=self)
