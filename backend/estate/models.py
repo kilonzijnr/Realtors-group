c449e870caf8e0ba08f48df3e89198fceb919638
0724654276 <you@example.com>
2022-01-12 11:36:14 +0300

    create a class model for rating

--- backend/estate/models.py
+++ backend/estate/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    review = models.TextField(null=True)
+    rate_property = models.PositiveSmallIntegerField(choices = RATE_CHOICES)
+    
