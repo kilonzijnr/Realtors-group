f8da5622255e48a3d21cf43d574a84420a38c3f0
0724654276 <you@example.com>
2022-01-12 12:18:49 +0300

    create views logic for rating

--- backend/estate/urls.py
+++ backend/estate/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns=[
   
-    path('rate/<project_id>/',views.rate,name ='rate'),
+    path('rate/<property_id>/',views.rate,name ='rate'),
     
 ]
--- backend/estate/views.py
+++ backend/estate/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Property
+from django.contrib.auth.decorators import login_required
+from .forms import  RateForm
+
 
 # Create your views here.
+@login_required
+def rate(request, project_id):
+    property = Property.objects.get(id=project_id)
+    user = request.user
+    if request.method == 'POST':
+        form = RateForm(request.POST)
+        if form.is_valid():
+            rate = form.save(commit=False)
+            rate.user = user
+            rate.project = property
+            rate.save()
+        return render(request, 'project.html', locals())
+    else:
+        form = RateForm()
+    return render(request, 'rate.html', locals())
